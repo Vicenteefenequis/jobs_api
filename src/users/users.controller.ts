8094589d9adb6023722dfbaab86666ad8f9f1a59
Vicente Nascimento <vicente_nascimento@sicredi.com.br>
2022-11-04 16:54:25 -0300

    feat: allowing tags api for swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, HttpStatus, Post, Response } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response as eResponse } from 'express';
 import { RegisterUser } from './infra/adapter/register-user';
+import { UserDTO } from './infra/dto/user-dto';
 import { CreateUserService } from './infra/service/create.service';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly createUserService: CreateUserService) {}
 
@@ -15,6 +22,8 @@ export class UsersController {
   })
   @ApiResponse({ status: 403, description: 'Forbidden.' })
   @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({ type: RegisterUser })
+  @ApiCreatedResponse({ type: UserDTO })
   async create(
     @Response() response: eResponse,
     @Body() register_user: RegisterUser,
