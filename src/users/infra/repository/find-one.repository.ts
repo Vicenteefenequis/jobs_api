19a2d974f8a7604e23e8e557bb786b39ab04add6
Vicente Nascimento <vicente_nascimento@sicredi.com.br>
2022-11-04 16:21:34 -0300

    feat: find one repository user

---
+++ src/users/infra/repository/find-one.repository.ts
@@ -0,0 +1,18 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/users/domain/namespace';
+import { User } from 'src/users/domain/user';
+import { Repository } from 'typeorm';
+import { UserModel } from './user.entity';
+import { UserMapper } from './user.mapper';
+
+export class FindOneRepository implements Users.FindOneRepository {
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly repository: Repository<UserModel>,
+  ) {}
+
+  async run(filter: Partial<Users.Model>): Promise<User> {
+    const orm_model = await this.repository.findOneBy(filter);
+    return UserMapper.modelToDomain(orm_model);
+  }
+}
